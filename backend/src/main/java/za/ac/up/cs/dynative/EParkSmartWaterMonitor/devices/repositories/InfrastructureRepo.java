3e82809fcff0a72eba742bb25a2a5b62c7d0044d
Janlu <u16014520@tuks.co.za>
2021-05-31 21:24:11 +0200

    Fixed incorrect repository paramater from InfrastructureRepo -> InfrastructureDevice

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/devices/repositories/InfrastructureRepo.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/devices/repositories/InfrastructureRepo.java
@@ -2,9 +2,10 @@ package za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.InfrastructureDevice;
 
 import java.util.UUID;
 
 @Repository("InfrastructureRepo")
-public interface InfrastructureRepo extends Neo4jRepository<InfrastructureRepo, UUID> {
+public interface InfrastructureRepo extends Neo4jRepository<InfrastructureDevice, UUID> {
 }
