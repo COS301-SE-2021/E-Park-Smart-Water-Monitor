18b5eac83ff6c609410490f987c12b2100bfa0e6
TheySeeMeRolan <u16002513@tuks.co.za>
2021-05-31 22:46:33 +0200

    mock test added to test travis with

--- backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/EParkSmartWaterMonitorApplicationTests.java
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/EParkSmartWaterMonitorApplicationTests.java
@@ -1,5 +1,8 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+import java.util.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +12,8 @@ class EParkSmartWaterMonitorApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void emptytest() {
 
+	}
 }
