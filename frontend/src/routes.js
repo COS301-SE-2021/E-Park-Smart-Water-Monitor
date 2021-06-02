bccbbd076769beba5b13f699587b75a02ad2f5cd
glitch-mich <u19014938@tuks.co.za>
2021-06-02 13:09:40 +0200

    Updated sidenav to navigate to Device page
    Added additional colours to the dashboard as well to match the green theme. The sidenav only includes a link to the new page to be able to view the changes but there is no way yet to navigate to the page via a component yet. Will need backend functionality populated components for that.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,30 +1,32 @@
 // core components
 import Dashboard from "views/admin/Dashboard.js";
-import Icons from "views/admin/Icons.js";
+// import Icons from "views/admin/Icons.js";
 import Login from "views/auth/Login.js";
-import Maps from "views/admin/Maps.js";
+// import Maps from "views/admin/Maps.js";
 import Profile from "views/admin/Profile.js";
-import Register from "views/auth/Register.js";
-import Tables from "views/admin/Tables.js";
+import Device from "views/admin/Device.js";
+
+// import Register from "views/auth/Register.js";
+// import Tables from "views/admin/Tables.js";
 // @material-ui/icons components
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import Dns from "@material-ui/icons/Dns";
-import FlashOn from "@material-ui/icons/FlashOn";
-import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
-import Grain from "@material-ui/icons/Grain";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Palette from "@material-ui/icons/Palette";
+// import FlashOn from "@material-ui/icons/FlashOn";
+// import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
+// import Grain from "@material-ui/icons/Grain";
+// import LocationOn from "@material-ui/icons/LocationOn";
+// import Palette from "@material-ui/icons/Palette";
 import Person from "@material-ui/icons/Person";
 import Tv from "@material-ui/icons/Tv";
 import VpnKey from "@material-ui/icons/VpnKey";
 
 var routes = [
-  {
-    href: "#pablo",
-    name: "Upgrade to pro",
-    icon: FlashOn,
-    upgradeToPro: true,
-  },
+  // {
+  //   href: "#pablo",
+  //   name: "Upgrade to pro",
+  //   icon: FlashOn,
+  //   upgradeToPro: true,
+  // },
   {
     path: "/index",
     name: "Dashboard",
@@ -33,36 +35,44 @@ var routes = [
     component: Dashboard,
     layout: "/admin",
   },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: Grain,
-    iconColor: "Primary",
-    component: Icons,
-    layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: LocationOn,
-    iconColor: "Warning",
-    component: Maps,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: Grain,
+  //   iconColor: "Primary",
+  //   component: Icons,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: LocationOn,
+  //   iconColor: "Warning",
+  //   component: Maps,
+  //   layout: "/admin",
+  // },
   {
     path: "/user-profile",
-    name: "User Profile",
+    name: "Profile",
     icon: Person,
     iconColor: "WarningLight",
     component: Profile,
     layout: "/admin",
   },
+  // {
+  //   path: "/tables",
+  //   name: "Tables",
+  //   icon: FormatListBulleted,
+  //   iconColor: "Error",
+  //   component: Tables,
+  //   layout: "/admin",
+  // },
   {
-    path: "/tables",
-    name: "Tables",
-    icon: FormatListBulleted,
-    iconColor: "Error",
-    component: Tables,
+    path: "/device",
+    name: "Device",
+    icon: AccountCircle,
+    iconColor: "ErrorLight",
+    component: Device,
     layout: "/admin",
   },
   {
@@ -74,31 +84,23 @@ var routes = [
     layout: "/auth",
   },
   {
-    path: "/register",
-    name: "Register",
-    icon: AccountCircle,
-    iconColor: "ErrorLight",
-    component: Register,
-    layout: "/auth",
-  },
-  {
     divider: true,
   },
   {
-    title: "Documentation",
+    title: "Profile",
   },
-  {
-    href:
-      "https://www.creative-tim.com/learning-lab/material-ui/overview/argon-dashboard?ref=admui-admin-sidebar",
-    name: "Getting started",
-    icon: FlashOn,
-  },
-  {
-    href:
-      "https://www.creative-tim.com/learning-lab/material-ui/colors/argon-dashboard?ref=admui-admin-sidebar",
-    name: "Foundation",
-    icon: Palette,
-  },
+  // {
+  //   href:
+  //     "https://www.creative-tim.com/learning-lab/material-ui/overview/argon-dashboard?ref=admui-admin-sidebar",
+  //   name: "Getting started",
+  //   icon: FlashOn,
+  // },
+  // {
+  //   href:
+  //     "https://www.creative-tim.com/learning-lab/material-ui/colors/argon-dashboard?ref=admui-admin-sidebar",
+  //   name: "Foundation",
+  //   icon: Palette,
+  // },
   {
     href:
       "https://www.creative-tim.com/learning-lab/material-ui/alerts/argon-dashboard?ref=admui-admin-sidebar",
