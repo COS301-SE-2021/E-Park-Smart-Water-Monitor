5d3e8a4fd1d4309a260e9e29191c30151fbc5899
Abel Jansen <abeljansenbf@gmail.com>
2021-06-03 20:28:25 +0200

    Added getSiteById unit test

--- backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/unit/WaterSiteUnitTest.java
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/unit/WaterSiteUnitTest.java
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.AddSiteRequest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.GetSiteByIdRequest;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.AddSiteResponse;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.GetSiteByIdResponse;
+
+import java.util.UUID;
 
 public class WaterSiteUnitTest extends UnitTestBaseClass {
     @Test
@@ -47,5 +51,42 @@ public class WaterSiteUnitTest extends UnitTestBaseClass {
 
         LOGGER.info("AddSiteResponse status: " + response.getStatus());
         LOGGER.info("AddSiteResponse success: " + response.getSuccess());
+    }
+
+    @Test
+    public void testGetSiteById() throws JsonProcessingException {
+        LOGGER.info("Testing GetSiteByIdRequest construction");
+
+        UUID siteId = UUID.randomUUID();
+
+        String jsonData = "{"
+                + "\"siteId\" : \""
+                + siteId
+                + "\""
+                + "}";
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        GetSiteByIdRequest request = mapper.readValue(jsonData, GetSiteByIdRequest.class);
+        assert (request != null);
+
+        LOGGER.info("DONE Testing GetSiteByIdRequest construction");
+
+        LOGGER.info("Testing getSiteById");
+
+        GetSiteByIdResponse response = waterSiteService.getSiteById(request);
+        assert (response != null);
+
+        LOGGER.info("DONE Testing getSiteById");
+
+        LOGGER.info("GetSiteByIdResponse status: " + response.getStatus());
+        LOGGER.info("GetSiteByIdResponse success: " + response.getSuccess());;
+
+        if (response.getSite() == null) {
+            LOGGER.info("GetSiteByIdResponse returned no water site");
+        }
+        else {
+            LOGGER.info("GetSiteByIdResponse site name: " + response.getSite().getWaterSiteName());
+        }
     }
 }
