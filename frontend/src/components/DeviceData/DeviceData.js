56a988dc332546c880f52f0968c4a8516c710dcf
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-06-03 19:39:08 +0200

    Device Data updates for displaying the table in dashboard.js

--- frontend/src/components/DeviceData/DeviceData.js
+++ frontend/src/components/DeviceData/DeviceData.js
@@ -1,57 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { makeStyles } from "@material-ui/core/styles";
-import componentStyles from "../../assets/theme/views/admin/dashboard";
-import TableCell from "@material-ui/core/TableCell";
-import TableRow from "@material-ui/core/TableRow";
+// import { makeStyles } from "@material-ui/core/styles";
+// import componentStyles from "../../assets/theme/views/admin/dashboard";
+// import TableCell from "@material-ui/core/TableCell";
+// import TableRow from "@material-ui/core/TableRow";
 
 export default function DeviceData() {
-    const [response, setResponse] = useState();
-    const useStyles = makeStyles(componentStyles);
-    const classes = useStyles();
+    const [response, setResponse] = useState({});
+    // const useStyles = makeStyles(componentStyles);
+    // const classes = useStyles();
 
     useEffect(() => {
         fetchResponse();
+        console.log(response.data);
     }, []);
 
     const fetchResponse = () => {
         axios
-            .post('localhost:8080/api/park/getParkWaterSites',
+            .post('http://localhost:8080/api/park/getParkWaterSites',
                 {parkId: "dda99bb7-4116-4e8a-ac35-9dad7e8c1b3f"})
             .then(res => setResponse(res))
     };
 
     return (
-        response.map((values)=>(
         <>
-            <TableRow>
-            <TableCell
-            classes={{
-              root:
-              classes.tableCellRoot +
-              " " +
-              classes.tableCellRootBodyHead,
-            }}
-            component="th"
-            variant="head"
-            scope="row"
-            >
-              {values.deviceName}
-            </TableCell>
-            <TableCell classes={{ root: classes.tableCellRoot }}>
-              {values.deviceData.latitude}
-            </TableCell>
-            <TableCell classes={{ root: classes.tableCellRoot }}>
-              {values.deviceData.longitude}
-            </TableCell>
-            <TableCell classes={{ root: classes.tableCellRoot }}>
-              {values.deviceData.deviceStatus}
-            </TableCell>
-            <TableCell classes={{ root: classes.tableCellRoot }}>
-              {values.deviceData.battery}
-            </TableCell>
-            </TableRow>
+        {/*    response?.map((values)=>(*/}
+        {/*        <TableRow>*/}
+        {/*        <TableCell*/}
+        {/*        classes={{*/}
+        {/*          root:*/}
+        {/*          classes.tableCellRoot +*/}
+        {/*          " " +*/}
+        {/*          classes.tableCellRootBodyHead,*/}
+        {/*        }}*/}
+        {/*        component="th"*/}
+        {/*        variant="head"*/}
+        {/*        scope="row"*/}
+        {/*        >*/}
+        {/*          {values.deviceName}*/}
+        {/*        </TableCell>*/}
+        {/*        <TableCell classes={{ root: classes.tableCellRoot }}>*/}
+        {/*          {values.deviceData.latitude}*/}
+        {/*        </TableCell>*/}
+        {/*        <TableCell classes={{ root: classes.tableCellRoot }}>*/}
+        {/*          {values.deviceData.longitude}*/}
+        {/*        </TableCell>*/}
+        {/*        <TableCell classes={{ root: classes.tableCellRoot }}>*/}
+        {/*          {values.deviceData.deviceStatus}*/}
+        {/*        </TableCell>*/}
+        {/*        <TableCell classes={{ root: classes.tableCellRoot }}>*/}
+        {/*          {values.deviceData.battery}*/}
+        {/*        </TableCell>*/}
+        {/*        </TableRow>*/}
+        {/*))*/}
         </>
-        ))
     );
 }
