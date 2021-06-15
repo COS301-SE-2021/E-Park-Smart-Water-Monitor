1e1cc8df561d7099c6a0d42047f1906deae2f8f7
Abel Jansen <abeljansenbf@gmail.com>
2021-06-16 00:04:10 +0200

    Added complete addInspection implementation

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/inspection/InspectionServiceImpl.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/inspection/InspectionServiceImpl.java
@@ -3,6 +3,10 @@ package za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.DevicesService;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.requests.FindDeviceRequest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.responses.FindDeviceResponse;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection.models.Inspection;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection.repositories.InspectionRepo;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection.requests.AddInspectionRequest;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection.requests.GetInspectionsRequest;
@@ -12,16 +16,41 @@ import za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection.responses.GetInspe
 @Service("InspectionServiceImpl")
 public class InspectionServiceImpl implements InspectionService {
 
-    private InspectionRepo inspectionRepo;
+    InspectionRepo inspectionRepo;
+    DevicesService devicesService;
 
     @Autowired
-    public InspectionServiceImpl(@Qualifier("InspectionRepo") InspectionRepo inspectionRepo) {
+    public InspectionServiceImpl(@Qualifier("InspectionRepo") InspectionRepo inspectionRepo, @Qualifier("DeviceServiceImpl") DevicesService devicesService) {
         this.inspectionRepo = inspectionRepo;
+        this.devicesService = devicesService;
     }
 
     @Override
     public AddInspectionResponse addInspection(AddInspectionRequest request) {
         AddInspectionResponse response = new AddInspectionResponse();
+
+        if (request.getDeviceId() != null) {
+            FindDeviceResponse findDeviceResponse = devicesService.findDevice(new FindDeviceRequest(request.getDeviceId()));
+
+            if (findDeviceResponse != null && findDeviceResponse.getSuccess()) {
+                Inspection inspection = new Inspection(findDeviceResponse.getDevice(), request.getDateDue(), request.getDescription());
+
+                inspectionRepo.save(inspection);
+
+                response.setStatus("Inspection succesfully added!");
+                response.setSuccess(true);
+            }
+            else {
+                response.setStatus("Failed to add inspection! Failure to get device!");
+                response.setSuccess(false);
+
+                return response;
+            }
+        }
+        else {
+            response.setStatus("Failed to add inspection! No deviceId specified!");
+            response.setSuccess(false);
+        }
 
         return  response;
     }
