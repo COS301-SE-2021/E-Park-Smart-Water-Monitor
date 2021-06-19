63110d8328fa708791eccae296051daac4aa4eef
Michaela Schormann <u19014938@tuks.co.za>
2021-06-19 19:34:44 +0200

    Clear old create park contents

--- frontend/src/components/Admin/AddParkBody.js
+++ frontend/src/components/Admin/AddParkBody.js
@@ -5,9 +5,6 @@ import { useTheme } from "@material-ui/core/styles";
 import componentStyles from "assets/theme/views/admin/admin";
 import "../../assets/css/addDevice.css";
 
-
-
-
 const useStyles = makeStyles(componentStyles);
 
 const AddParkBody = () => {
@@ -17,25 +14,7 @@ const AddParkBody = () => {
 
     return (
         <>
-            <div>
-                <br/>
-                <label id="name" for="Name">Name</label>
-                <br/>
-                <input type="text" name="Name" />
-                <br/>
-            </div>
 
-            <div>
-                <br/><br/>
-                <label id="lat" htmlFor="Latitude">Latitude</label>
-                <br/>
-                <input type="text" name="latitude"/>
-                <br/><br/><br/>
-                <label id="long" htmlFor="Longitude">Longitude</label>
-                <br/>
-                <input type="text" name="longitude"/>
-            </div>
-            <br/><br/><br/>
         </>
     );
 };
