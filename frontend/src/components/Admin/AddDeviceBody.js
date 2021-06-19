79825f21b8a2c31c2289aa5f8ab6794ff22d929e
Michaela Schormann <u19014938@tuks.co.za>
2021-06-19 19:29:40 +0200

    Add map to creating a new device

--- frontend/src/components/Admin/AddDeviceBody.js
+++ frontend/src/components/Admin/AddDeviceBody.js
@@ -9,11 +9,17 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Select from "react-select";
 import axios from "axios";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 // import AdminModal from 'admin-modal.js'
 
 
 
 const useStyles = makeStyles(componentStyles);
+
+const mapStyles = {
+    width: `100%`,
+    height: `100%`
+};
 
 const AddDeviceBody = () => {
     const classes = useStyles();
@@ -120,7 +126,25 @@ const AddDeviceBody = () => {
                     </Col>
                 </Row>
 
-                
+                <Row>
+                    <Col>
+                        <div style={ { height: 250 } } className="mb-3" >
+                            {/*rietvlei centre*/}
+                            {latitude && longitude && <MapContainer style={mapStyles} center={[-25.88536975144579, 28.277796392845673]} zoom={13} scrollWheelZoom={false}>
+                                <TileLayer
+                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={[latitude, longitude]}>
+                                    <Popup>
+                                        <h3>{name}</h3>
+                                        location
+                                    </Popup>
+                                </Marker>
+                            </MapContainer>}
+                        </div>
+                    </Col>
+                </Row>
 
 
                 <Button background-color="primary" variant="primary" type="submit">
