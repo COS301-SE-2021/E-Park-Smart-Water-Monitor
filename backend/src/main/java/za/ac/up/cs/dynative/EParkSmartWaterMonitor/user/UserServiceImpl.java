da68c0e74eeb8a64c73c332cfb7fb7860b849ddf
TheySeeMeRolan <u16002513@tuks.co.za>
2021-06-20 22:58:26 +0200

    @Lazy added to UserServiceImpl

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/UserServiceImpl.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/UserServiceImpl.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.notification.NotificationService;
@@ -35,7 +36,7 @@ public class UserServiceImpl implements UserService {
     @Autowired
     public UserServiceImpl(@Qualifier("UserRepo") UserRepo userRepo,
                            @Qualifier("ParkService") ParkService parkService,
-                            @Qualifier("NotificationServiceImpl") NotificationService notificationService) {
+                           @Lazy @Qualifier("NotificationServiceImpl") NotificationService notificationService) {
         this.userRepo = userRepo;
         this.parkService = parkService;
         this.notificationService = notificationService;
