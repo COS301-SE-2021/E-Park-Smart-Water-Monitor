0deb8f39a29b28c189b946b09ea67260a1840e93
Michaela Schormann <u19014938@tuks.co.za>
2021-07-10 20:05:56 +0200

    Edit Device component setup for when the request is prepared

--- frontend/src/components/Admin/Device/EditDeviceBody.js
+++ frontend/src/components/Admin/Device/EditDeviceBody.js
@@ -13,7 +13,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import axios from "axios";
-import {Marker, Popup} from "react-leaflet";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 const { Form } = require( "react-bootstrap" );
 
 
@@ -21,97 +21,44 @@ const { Form } = require( "react-bootstrap" );
 const useStyles = makeStyles(componentStyles);
 
 const EditDeviceBody = (props) => {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [park, setPark] = useState("")
-    const [idNumber, setIDNumber] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
     const [name, setName] = useState("")
-    const [surname, setSurname] = useState("")
-    const [username, setUsername] = useState("")
-    const [role, setRole] = useState("")
-    const [cellNumber, setCellNumber] = useState("")
-    const [parkOptions, setParkOptions] = useState("")
-    const [error, setError] = useState("")
+    const [park, setPark] = useState("") // id and name stored
+    const [site, setSite] = useState("") // id and name stored
+    const [model, setModel] = useState("ESP32")
+    const [latitude, setLatitude] = useState(-25.899494434)
+    const [longitude, setLongitude] = useState(28.280765508)
 
-    let userRoles = [
-        { value: 'ADMIN', label: 'Admin' },
-        { value: 'FIELD_ENGINEER', label: 'Field Engineer' },
-        { value: 'RANGER', label: 'Ranger' }
-    ];
+
 
     useEffect(() => {
 
         // add the props to the variables so that the user can change the values in the components
-        if(props && props.userDetails)
+        if(props && props.deviceDetails)
         {
-            // set the role to be the same object notation with value and label
-            let oldRole = props.userDetails.role
-            for(let i =0; i< userRoles.length; i++){
-                if(oldRole == userRoles[i].value){
-                    setRole(userRoles[i])
-                }
-            }
-
-            let p = props.userDetails
-
-            let cell = p.cellNumber;
-            cell = cell.substr(4)
-            cell = '0'+cell
-
-            setPark(p.park)
-            setIDNumber(p.idNumber)
-            setEmail(p.email)
-            setPassword(p.password)
-            setName(p.name)
-            setSurname(p.surname)
-            setUsername(p.username)
-            // setRole(p.role)
-            setCellNumber(cell)
+            // set all your device details here
+            setName(props.deviceDetails.deviceName)
         }
-    },[props.userDetails])
+    },[props.deviceDetails])
 
     const submit = (e) => {
         e.preventDefault()
 
-        if(props && props.userDetails)
+        if(props && props.deviceDetails)
         {
-            // accomodate for provided email is already in use error
-            let temp_email = email
-            if(email === props.userDetails.email)
-            {
-                //clear the value
-                temp_email = ""
-            }
-
-            let temp_username = username
-            if(username === props.userDetails.username)
-            {
-                //clear the value
-                temp_username = ""
-            }
-
+            // device edit request
             let obj = {
-                username: props.userDetails.username,
-                idNumber: idNumber,
-                email: temp_email ,
-                name: name,
-                surname: surname,
-                newUsername: temp_username,
-                role: role.value,
-                cellNumber: cellNumber
+
             }
 
 
-            axios.post('http://localhost:8080/api/user/editUser', obj
+            axios.post('http://localhost:8080/api/user/editDevice', obj
             ).then((res)=>{
 
                 console.log("response:"+JSON.stringify(res))
                 if(res.data.success == "false")
                 {
                     setError(res.data.status)
-                    console.log("error with editing user")
+                    console.log("error with editing device")
                 }else{
                     window.location.reload(); //need to get the new data from the db to populate the table again
                 }
@@ -125,39 +72,14 @@ const EditDeviceBody = (props) => {
     }
 
 
-
     return (
         <>
             <Form onSubmit={ submit }>
                 <Row>
                     <Col>
-                        <Form.Label>Role</Form.Label>
-                        <Select required={"required"} className="mb-3" name="role" options={ userRoles } value={role} onChange={e => setRole(e)}/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Group className="mb-3" controlId="email" >
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control required={"required"} type="email" placeholder="Enter email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
-                            <Form.Text className="text-muted">
-                                An email will be sent to the user informing them of their registration on the system.
-                            </Form.Text>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
                         <Form.Group className="mb-3" >
-                            <Form.Label>Firstname</Form.Label>
-                            <Form.Control required={"required"} type="text" placeholder="Firstname" name="name" value={name} onChange={e => setName(e.target.value)}/>
-                        </Form.Group>
-
-                    </Col>
-                    <Col>
-                        <Form.Group className="mb-3" >
-                            <Form.Label>Surname</Form.Label>
-                            <Form.Control required={"required"} type="text" placeholder="Surname" name="surname" value={surname} onChange={e => setSurname(e.target.value)}/>
+                            <Form.Label>Device Name</Form.Label>
+                            <Form.Control required={"required"} type="text" placeholder="Device Name" name="name" value={name} onChange={e => setName(e.target.value)}/>
                         </Form.Group>
 
                     </Col>
@@ -165,35 +87,42 @@ const EditDeviceBody = (props) => {
                 <Row>
                     <Col>
                         <Form.Group className="mb-3" >
-                            <Form.Label>National Identification Number</Form.Label>
-                            <Form.Control required={"required"} type="text" minLength={13} maxLength={13} pattern="[0-9]*" placeholder="ID Number" name="id_number" value={idNumber} onChange={e => setIDNumber(e.target.value)}/>
-                        </Form.Group>
-
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col>
-                        <Form.Group className="mb-3" >
-                            <Form.Label>Cell Number</Form.Label>
-                            <Form.Control required={"required"} type="text" minLength={10} maxLength={10} pattern="[0-9]*" placeholder="Cell Number" name="cell_number" value={cellNumber} onChange={e => setCellNumber(e.target.value)}/>
+                            <Form.Label>Latitude</Form.Label>
+                            <Form.Control required={"required"} type="number" step={0.001} placeholder="latitude" name="latitude" value={latitude} onChange={e => setLatitude(e.target.value)}/>
                         </Form.Group>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
                         <Form.Group className="mb-3" >
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control required={"required"} type="text" placeholder="Username" name="username" value={username} onChange={e => setUsername(e.target.value)}/>
+                            <Form.Label>Longitude</Form.Label>
+                            <Form.Control required={"required"} type="number" step={0.001} placeholder="longitude" name="longitude" value={longitude} onChange={e => setLongitude(e.target.value)}/>
                         </Form.Group>
+                    </Col>
+                </Row>
 
+                <Row>
+                    <Col>
+                        <div style={ { height: 250 } } className="mb-3" >
+                            {/*rietvlei centre*/}
+                            {latitude && longitude && <MapContainer style={mapStyles} center={[-25.88536975144579, 28.277796392845673]} zoom={13} scrollWheelZoom={false}>
+                                <TileLayer
+                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={[latitude, longitude]}>
+                                    <Popup>
+                                        <h3>{name}</h3>
+                                        location
+                                    </Popup>
+                                </Marker>
+                            </MapContainer>}
+                        </div>
                     </Col>
                 </Row>
 
 
-
-
-                <Button variant="primary" type="submit" >
+                <Button background-color="primary" variant="primary" type="submit">
                     Edit
                 </Button>
             </Form>
