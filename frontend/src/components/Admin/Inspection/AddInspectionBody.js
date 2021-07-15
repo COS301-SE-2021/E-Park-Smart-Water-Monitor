317909a5b44728926bdfc2f8b5acd23db2a436ba
Michaela Schormann <u19014938@tuks.co.za>
2021-07-15 14:55:29 +0200

    Added validation to inspection

--- frontend/src/components/Admin/Inspection/AddInspectionBody.js
+++ frontend/src/components/Admin/Inspection/AddInspectionBody.js
@@ -53,24 +53,15 @@ const AddInspectionBody = (props) => {
     return (
         <>
             <Form onSubmit={handleSubmit}>
-                {/*<Form.Group controlId="formBasicEmail">*/}
-                {/*    <Form.Label>Device</Form.Label>*/}
-                {/*    <Form.Control type="text" placeholder="Enter device id" onChange={selectDevice}/>*/}
-                {/*</Form.Group>*/}
-
-                {/* <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Site</Form.Label>
-                    <Form.Control type="text" placeholder="Enter a site id" onChange={selectSite}/>
-                </Form.Group> */}
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Description</Form.Label>
-                    <Form.Control type="text" placeholder="Enter a description" onChange={selectDescription}/>
+                    <Form.Control type="text" Required={"required"} placeholder="Enter a description" onChange={selectDescription}/>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Due date</Form.Label>
-                    <Form.Control type="date" placeholder="Enter a due date" onChange={selectDate}/>
+                    <Form.Control type="date" Required={"required"}  placeholder="Enter a due date" onChange={selectDate}/>
                 </Form.Group>
 
                 <Button background-color="primary" variant="primary" type="submit">
