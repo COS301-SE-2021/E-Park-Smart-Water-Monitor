c3a1f00ae75c3df352a52550b596018c7f3df06f
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-07-30 13:56:58 +0200

    Repo added for the user authentication

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/security/ApplicationSecurity.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/security/ApplicationSecurity.java
@@ -1,14 +1,20 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -16,20 +22,21 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.user.repositories.UserRepo;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-
+/*
     private final RequestMatcher USER_URLS = new OrRequestMatcher(
             new AntPathRequestMatcher("/api/user/login"),
             new AntPathRequestMatcher("/api/**"),
             new AntPathRequestMatcher("/api/user/createUser")
-);
+);*/
 
-    private final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(USER_URLS);
+   // private final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(USER_URLS);
 
     private final JwtAuthProvider provider;
 
@@ -38,13 +45,16 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         this.provider = provider;
     }
 
-    @Override
+    @Autowired
+    private UserRepo repo;
+
+   /* @Override
     public void configure(WebSecurity web) {
         web.ignoring().requestMatchers(USER_URLS);
-    }
+    }*/
 
 
-    @Override
+/*    @Override
     public void configure(HttpSecurity http) throws Exception {
         http
                 .cors().and().csrf().disable()
@@ -56,9 +66,54 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
                 .and()
                 .authenticationProvider(provider)
                 .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class);
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailService(repo);
     }
 
     @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyAuthority("ADMIN", "FIELD_ENGINEER", "RANGER")
+                .antMatchers("/api/devices/addDevice").hasAnyAuthority("ADMIN", "FIELD_ENGINEER")
+                .antMatchers("/api/devices/editDevice").hasAnyAuthority("ADMIN", "FIELD_ENGINEER")
+                .antMatchers("addInspection/addInspection").hasAnyAuthority("ADMIN", "FIELD_ENGINEER")
+                .antMatchers("/api/park/addPark").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/park/editPark").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/user/createUser").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/user/deleteUser").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/user/editUser").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/sites/addSite").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/user/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    /*@Bean
     JwtAuthFilter restAuthenticationFilter() throws Exception {
         final JwtAuthFilter filter = new JwtAuthFilter(PROTECTED_URLS);
         filter.setAuthenticationManager(authenticationManager());
@@ -71,5 +126,5 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
         successHandler.setRedirectStrategy(new NoRedirectStrategy());
         return successHandler;
-    }
+    }*/
 }
