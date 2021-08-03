d50a2ea2920a973fcd0d5358c8dc0a5d96019250
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-08-03 16:14:20 +0200

    Reset password request complete

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/requests/SetPasswordRequest.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/requests/SetPasswordRequest.java
@@ -1,7 +1,36 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.user.requests;
 
-public class SetPasswordRequest {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class SetPasswordRequest {
+    private String username;
     private String password1;
     private String password2;
+
+    public String getPassword1() {
+        return password1;
+    }
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+    public String getPassword2() {
+        return password2;
+    }
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public SetPasswordRequest(@JsonProperty("username") String username,@JsonProperty("password1") String password1,
+                              @JsonProperty("password2") String password2){
+        this.username= username;
+        this.password1= password1;
+        this.password2= password2;
+    }
 }
