7a6222a85d1cc2a53a0de7a0775c7a15a4702a47
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-08-03 16:25:24 +0200

    attribute name changes in reset password

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/requests/SetPasswordRequest.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/requests/SetPasswordRequest.java
@@ -4,20 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SetPasswordRequest {
     private String username;
-    private String password1;
-    private String password2;
+    private String newPassword;
+    private String confirmPassword;
 
-    public String getPassword1() {
-        return password1;
+    public String getNewPassword() {
+        return newPassword;
     }
-    public void setPassword1(String password1) {
-        this.password1 = password1;
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
     }
-    public String getPassword2() {
-        return password2;
+    public String getConfirmPassword() {
+        return confirmPassword;
     }
-    public void setPassword2(String password2) {
-        this.password2 = password2;
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
     public String getUsername() {
         return username;
@@ -27,10 +27,10 @@ public class SetPasswordRequest {
     }
 
 
-    public SetPasswordRequest(@JsonProperty("username") String username,@JsonProperty("password1") String password1,
-                              @JsonProperty("password2") String password2){
+    public SetPasswordRequest(@JsonProperty("username") String username,@JsonProperty("newPassword") String newPassword,
+                              @JsonProperty("confirmPassword") String confirmPassword){
         this.username= username;
-        this.password1= password1;
-        this.password2= password2;
+        this.newPassword= newPassword;
+        this.confirmPassword= confirmPassword;
     }
 }
