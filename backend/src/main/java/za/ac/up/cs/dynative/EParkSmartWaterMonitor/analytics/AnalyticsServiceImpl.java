9cb0f430b4da9cc08feede494c6173b2004dbb29
Janlu <u16014520@tuks.co.za>
2021-08-06 14:09:07 +0200

    Added AnalyticsServiceImpl

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/analytics/AnalyticsServiceImpl.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/analytics/AnalyticsServiceImpl.java
@@ -1,4 +1,24 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics;
 
-public class AnalyticsServiceImpl {
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics.requests.DeviceProjectionRequest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics.responses.DeviceProjectionResponse;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.DevicesService;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.DevicesServicesImpl;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories.DeviceRepo;
+
+@Service("AnalyticsServiceImpl")
+public class AnalyticsServiceImpl implements AnalyticsService {
+
+    private DevicesService devicesService;
+
+    public AnalyticsServiceImpl(@Qualifier("DeviceServiceImpl") DevicesService devicesService) {
+        this.devicesService = devicesService;
+    }
+
+    @Override
+    public DeviceProjectionResponse deviceProjection(DeviceProjectionRequest deviceProjectionRequest) {
+        return null;
+    }
 }
