f7993e5b7f17dc4cd79c982220597c46eb537cb1
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-08-09 14:18:10 +0200

    Reset password finalize response completed.

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/responses/ResetPasswordFinalizeResponse.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/responses/ResetPasswordFinalizeResponse.java
@@ -1,4 +1,26 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.user.responses;
 
 public class ResetPasswordFinalizeResponse {
+    private String message;
+    private boolean success;
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public ResetPasswordFinalizeResponse(String message, boolean success){
+        this.message= message;
+        this.success= success;
+    }
+
 }
