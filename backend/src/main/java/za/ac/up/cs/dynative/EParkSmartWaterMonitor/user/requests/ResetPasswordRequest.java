3c36c518d3278c4dcb3422c6f2e500eeb6782c4d
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-08-09 13:19:52 +0200

    Reset password request complete

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/requests/ResetPasswordRequest.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/requests/ResetPasswordRequest.java
@@ -1,4 +1,18 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.user.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ResetPasswordRequest {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public ResetPasswordRequest(@JsonProperty("username") String username){
+        this.username = username;
+    }
 }
