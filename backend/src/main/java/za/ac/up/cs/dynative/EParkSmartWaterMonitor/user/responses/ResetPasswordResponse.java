26d82a7d41b2284215aac08191e479582b449f47
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-08-09 14:14:53 +0200

    Reset password response completed.

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/responses/ResetPasswordResponse.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/user/responses/ResetPasswordResponse.java
@@ -1,4 +1,16 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.user.responses;
 
 public class ResetPasswordResponse {
+    private String code;
+
+    public String getCode(){
+        return code;
+    }
+    public void setCode(String code){
+        this.code= code;
+    }
+
+    public ResetPasswordResponse(String code){
+        this.code= code;
+    }
 }
