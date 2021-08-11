eb08477298fee6c231e50d3557c59619127c59ea
Joanita <73602735+JoanitaNell@users.noreply.github.com>
2021-08-11 17:04:08 +0200

    Devices unit tests:   --> request to get device data cleaned up

--- backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/GetDeviceData.java
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/GetDeviceData.java
@@ -14,7 +14,6 @@ import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories.Measurem
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories.DeviceRepo;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.requests.GetDeviceDataRequest;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.responses.GetDeviceDataResponse;
-import za.ac.up.cs.dynative.EParkSmartWaterMonitor.exceptions.InvalidRequestException;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.ParkServiceImpl;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.WaterSiteServicesImpl;
 
