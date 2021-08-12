3e956a6bc5a51fd03d9b8093d7f6913d5fb71916
TheySeeMeRolan <u16002513@tuks.co.za>
2021-08-12 19:52:12 +0200

    DataNotificationRequest added

---
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/devices/requests/DataNotificationRequest.java
@@ -0,0 +1,43 @@
+package za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.requests;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.Measurement;
+
+import java.util.List;
+
+public class DataNotificationRequest {
+
+    String deviceName;
+    List<Measurement> measurements;
+
+    public DataNotificationRequest(
+            @JsonProperty("deviceName") String deviceName,
+            @JsonProperty("measurements")  List<Measurement> measurements) {
+        this.deviceName = deviceName;
+        this.measurements = measurements;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public List<Measurement> getMeasurements() {
+        return measurements;
+    }
+
+    public void setMeasurements(List<Measurement> measurements) {
+        this.measurements = measurements;
+    }
+
+    @Override
+    public String toString() {
+        return "DataNotificationRequest{" +
+                "deviceName='" + deviceName + '\'' +
+                ", measurements=" + measurements +
+                '}';
+    }
+}
