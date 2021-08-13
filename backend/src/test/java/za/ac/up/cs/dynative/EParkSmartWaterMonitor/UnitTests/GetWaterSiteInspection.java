b20f3c78db1002f95b0c3cd06a4b4c9d823bc6cd
JoanitaNell <u19006812@tuks.co.za>
2021-08-13 12:07:58 +0200

    Inspections unit tests:   --> set up the basics structure for get all inspections, set inspection comments, get device inspections, get water site inspections, and add inspections

---
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/GetWaterSiteInspection.java
@@ -0,0 +1,32 @@
+package za.ac.up.cs.dynative.EParkSmartWaterMonitor.UnitTests;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.DevicesService;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection.InspectionServiceImpl;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.inspection.repositories.InspectionRepo;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.repositories.ParkRepo;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.WaterSiteService;
+
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class GetWaterSiteInspection {
+
+    @Mock
+    private InspectionRepo inspectionRepo;
+    @Mock
+    private ParkRepo parkRepo;
+
+    @Mock(name = "DeviceServiceImpl")
+    private DevicesService devicesService;
+    @Mock(name = "WaterSiteServiceImpl")
+    private WaterSiteService waterSiteService;
+
+    @InjectMocks
+    private InspectionServiceImpl inspectionService;
+}
+
