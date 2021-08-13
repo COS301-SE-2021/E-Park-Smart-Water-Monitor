f204b676a50f727e5d8b84403211f24a987b6507
JoanitaNell <u19006812@tuks.co.za>
2021-08-13 10:39:40 +0200

    Device unit tests:   --> delete device setup

---
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/DeleteDevice.java
@@ -0,0 +1,47 @@
+package za.ac.up.cs.dynative.EParkSmartWaterMonitor.UnitTests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.DevicesServicesImpl;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.Device;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories.MeasurementRepo;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories.DeviceRepo;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.ParkServiceImpl;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.WaterSiteServicesImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class DeleteDevice {
+
+    @Mock
+    private DeviceRepo deviceRepo;
+    @Mock
+    private MeasurementRepo measurementRepo;
+
+    @Mock(name = "parkService")
+    private ParkServiceImpl parkService;
+    @Mock(name = "waterSiteService")
+    private WaterSiteServicesImpl waterSiteServices;
+
+    @InjectMocks
+    private DevicesServicesImpl devicesServices;
+
+    @Test
+    @DisplayName("Attempt to delete a device but the id is null")
+    public void DeleteDeviceNullID() {
+
+    }
+}
