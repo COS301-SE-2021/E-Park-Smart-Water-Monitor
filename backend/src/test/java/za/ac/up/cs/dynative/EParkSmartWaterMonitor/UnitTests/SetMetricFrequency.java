620d38ae2bcc20107769c0cc5433e9dd0c0f1f3e
JoanitaNell <u19006812@tuks.co.za>
2021-08-13 11:25:59 +0200

    Device unit tests:   --> set metric frequency but device does not exist

--- backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/SetMetricFrequency.java
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/SetMetricFrequency.java
@@ -5,15 +5,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.DevicesServicesImpl;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.Device;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories.MeasurementRepo;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.repositories.DeviceRepo;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.requests.DeleteDeviceRequest;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.requests.SetMetricFrequencyRequest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.responses.DeleteDeviceResponse;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.responses.SetMetricFrequencyResponse;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.ParkServiceImpl;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.WaterSiteServicesImpl;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,5 +48,19 @@ public class SetMetricFrequency {
         assertNotNull(response);
         assertEquals(false,response.getSuccess());
         assertEquals("No device id specified.",response.getStatus());
+    }
+
+    @Test
+    @DisplayName("Attempt to set the frequency but the device does not exist.")
+    public void SetMetricFrequencyDeviceDNE() {
+        //setup
+        Optional<Device> device = Optional.empty();
+        Mockito.when(deviceRepo.findById(Mockito.any())).thenReturn(device);
+
+        //test
+        SetMetricFrequencyResponse response = devicesServices.setMetricFrequency(new SetMetricFrequencyRequest(UUID.randomUUID(),12));
+        assertNotNull(response);
+        assertEquals(false,response.getSuccess());
+        assertEquals("No device configurations set.",response.getStatus());
     }
 }
