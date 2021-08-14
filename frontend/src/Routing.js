750c97b2d692283ec690be2939ca0439b6e8114e
Michaela Schormann <u19014938@tuks.co.za>
2021-08-14 16:34:23 +0200

    Non-admin gets rerouted to login

--- frontend/src/Routing.js
+++ frontend/src/Routing.js
@@ -14,7 +14,7 @@ const Routing = () => {
             <Switch>
                 <Route path="/auth/index" render={(props) => <AuthLayout {...props}/>} />
                 <Route path="/dashboard/index" render={(props) => (user.isLogin()? <DashboardLayout {...props} /> : <Redirect to={"/auth/index"} /> ) } />
-                <Route path="/admin/index" render={(props) => (user.isLogin()? <AdminLayout {...props} />: <Redirect to={"/auth/index"} /> ) } />
+                <Route path="/admin/index" render={(props) => ( (user.isLogin() && (user.role === "ADMIN")) ? <AdminLayout {...props} />: <Redirect to={"/auth/index"} /> ) } />
                 <Redirect from="/" to="/auth/index" />
             </Switch>
         </BrowserRouter>
