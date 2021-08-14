567e3dfe61a2aa8758af946f1775c4785f02cc5d
JoanitaNell <u19006812@tuks.co.za>
2021-08-14 20:23:12 +0200

    Park unit tests:   --> Find park by id where the id is null

--- backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/FindParkById.java
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/FindParkById.java
@@ -1,4 +1,5 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.UnitTests;
+
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
@@ -41,7 +42,6 @@ public class FindParkById {
     String name2="UnitTest 2";
     Set<WaterSite> siteSet1;
     Set<WaterSite> siteSet2;
-
     int id21= 336;
     int id22= 337;
     WeatherData weather1;
@@ -50,27 +50,25 @@ public class FindParkById {
     double temp2=28;
     String moonPhase1 = "Growing";
     String moonPhase2 = "Full";
-    double humudity1= 39;
-    double humudity2= 37;
+    double humidity1= 39;
+    double humidity2= 37;
     double windSpeed1 =16;
     double windSpeed2 =20;
     Date date1= new Date();
     Date date2= new Date();
-
     Set<WeatherData> weatherSet1;
     Set<WeatherData> weatherSet2;
-
     Park park1;
     Park park2;
 
     @Test
-    @DisplayName("Find a park by id, but id is null")
-    public void findParkNull(){
+    @DisplayName("Find a park by id, but the id is null")
+    public void findParkIdNull(){
         FindByParkIdRequest request= new FindByParkIdRequest(null);
-        Throwable t= assertThrows(InvalidRequestException.class,()->parkService.findByParkId(request));
-        assertEquals("No park id specified",t.getMessage());
+        FindByParkIdResponse response =parkService.findByParkId(request);
+        assertNotNull(response);
+        assertEquals(false,response.getSuccess());
     }
-
 
     @Test
     @DisplayName("Find a nonexisting park")
@@ -85,26 +83,26 @@ public class FindParkById {
 
     @Test
     @DisplayName("Find an existing park.")
-    public void FindParkExisting() throws InvalidRequestException {
+    public void FindParkExisting(){
+        //setup
         site1= new WaterSite(id1,name1,lat1,lon1);
         site2= new WaterSite(id2,name2,lat2,lon2);
         siteSet1= new HashSet<>();
         siteSet2= new HashSet<>();
         siteSet1.add(site1);
         siteSet2.add(site2);
-
-        weather1= new WeatherData(id21,temp1,moonPhase1,humudity1,windSpeed1,date1);
-        weather2= new WeatherData(id22,temp2,moonPhase2,humudity2,windSpeed2,date2);
+        weather1= new WeatherData(id21,temp1,moonPhase1,humidity1,windSpeed1,date1);
+        weather2= new WeatherData(id22,temp2,moonPhase2,humidity2,windSpeed2,date2);
         weatherSet1 = new HashSet<>();
         weatherSet2 = new HashSet<>();
         weatherSet1.add(weather1);
         weatherSet2.add(weather2);
-
         park1= new Park("Unit Test Park 1",-27.378888,28.111471,weatherSet1,siteSet1);
         park2= new Park("Unit Test Park 2",-27.368888,28.681111,weatherSet2,siteSet2);
         List<Park> p= new ArrayList<>();
         Mockito.when(parkRepo.findParkById(park1.getId())).thenReturn(park1);
 
+        //test
         FindByParkIdRequest request= new FindByParkIdRequest(park1.getId());
         FindByParkIdResponse response= parkService.findByParkId(request);
         assertNotNull(response);
