5525bf43749474e513e16bb0e0db47f09bbe3535
TheySeeMeRolan <u16002513@tuks.co.za>
2021-08-14 15:03:34 +0200

    DataNotificationQuery added

---
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/devices/models/DataNotificationQuery.java
@@ -0,0 +1,34 @@
+package za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DataNotificationQuery {
+    String deviceName;
+    WaterSourceData waterSourceData;
+    String timestamp;
+
+
+    public DataNotificationQuery(@JsonProperty("deviceName") String deviceName, @JsonProperty("WaterSourceData") WaterSourceData waterSourceData,    @JsonProperty("timestamp") String timestamp)
+    {
+        this.deviceName = deviceName;
+        this.waterSourceData = waterSourceData;
+        this.timestamp=timestamp;
+
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public WaterSourceData getWaterSourceData() {
+        return waterSourceData;
+    }
+
+    public void setWaterSourceData(WaterSourceData waterSourceData) {
+        this.waterSourceData = waterSourceData;
+    }
+}
--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/devices/requests/DataNotificationRequest.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/devices/requests/DataNotificationRequest.java
@@ -1,49 +1,25 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.DataNotificationQuery;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.WaterSourceData;
+
+import java.util.List;
 
 public class DataNotificationRequest {
 
-    //'deviceName':deviceName,
-//        'timestamp':date,
-//        'WaterSourceData':{
-//    'deviceName':deviceName,
-//            'measurements':newMeasurements
+    List<DataNotificationQuery> data;
 
-    String deviceName;
-    String timestamp;
-    WaterSourceData WaterSourceData;
-
-    public DataNotificationRequest(@JsonProperty("deviceName") String deviceName,
-                                   @JsonProperty("timestamp") String timestamp,
-                                   @JsonProperty("WaterSourceData") za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.WaterSourceData waterSourceData) {
-        this.deviceName = deviceName;
-        this.timestamp = timestamp;
-        WaterSourceData = waterSourceData;
+    public DataNotificationRequest(List<DataNotificationQuery> incomingData)
+    {
+        data=incomingData;
     }
 
-    public String getDeviceName() {
-        return deviceName;
+    public List<DataNotificationQuery> getData() {
+        return data;
     }
 
-    public void setDeviceName(String deviceName) {
-        this.deviceName = deviceName;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.WaterSourceData getWaterSourceData() {
-        return WaterSourceData;
-    }
-
-    public void setWaterSourceData(za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.models.WaterSourceData waterSourceData) {
-        WaterSourceData = waterSourceData;
+    public void setData(List<DataNotificationQuery> data) {
+        this.data = data;
     }
 }
