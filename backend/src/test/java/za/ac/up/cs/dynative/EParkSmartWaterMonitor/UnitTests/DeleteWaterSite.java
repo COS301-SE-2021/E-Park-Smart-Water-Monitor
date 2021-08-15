7962f4bd8f8a9eb8e6b4c12456c368781d91afa6
JoanitaNell <u19006812@tuks.co.za>
2021-08-15 12:28:39 +0200

    Water site unit test:   --> Delete a water site with success   --> Delete a water site that does not exist   --> Delete a water site but no id is specified

--- backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/DeleteWaterSite.java
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/UnitTests/DeleteWaterSite.java
@@ -8,14 +8,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import za.ac.up.cs.dynative.EParkSmartWaterMonitor.exceptions.InvalidRequestException;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.ParkServiceImpl;
-import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.models.Park;
-import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.responses.FindByParkIdResponse;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.WaterSiteServicesImpl;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.models.WaterSite;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.repositories.WaterSiteRepo;
-import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.AddSiteRequest;
-import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.AddSiteResponse;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.DeleteWaterSiteRequest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.DeleteWaterSiteResponse;
 
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,4 +30,42 @@ public class DeleteWaterSite {
 
     @InjectMocks
     WaterSiteServicesImpl waterSiteServices;
+
+    @Test
+    @DisplayName("Attempt to delete a water site but no id is specified")
+    public void DeleteWaterSiteIdNull(){
+        DeleteWaterSiteRequest request = new DeleteWaterSiteRequest(null);
+        DeleteWaterSiteResponse response = waterSiteServices.deleteWaterSite(request);
+        assertNotNull(response);
+        assertEquals(false, response.getSuccess());
+        assertEquals("No watersite id specified.", response.getStatus());
+    }
+
+    @Test
+    @DisplayName("Attempt to delete a water site that does not exist")
+    public void DeleteWaterSiteDNE(){
+        //setup
+        Mockito.when(repo.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        //test
+        DeleteWaterSiteRequest request = new DeleteWaterSiteRequest(UUID.randomUUID());
+        DeleteWaterSiteResponse response = waterSiteServices.deleteWaterSite(request);
+        assertNotNull(response);
+        assertEquals(false, response.getSuccess());
+        assertEquals("No watersite with this id exists.", response.getStatus());
+    }
+
+    @Test
+    @DisplayName("Attempt to delete a water site and succeed")
+    public void DeleteWaterSiteSuccess(){
+        //setup
+        Mockito.when(repo.findById(Mockito.any())).thenReturn(Optional.of(new WaterSite()));
+
+        //test
+        DeleteWaterSiteRequest request = new DeleteWaterSiteRequest(UUID.randomUUID());
+        DeleteWaterSiteResponse response = waterSiteServices.deleteWaterSite(request);
+        assertNotNull(response);
+        assertEquals(true, response.getSuccess());
+        assertEquals("Successfully deleted the watersite and all related entities.", response.getStatus());
+    }
 }
