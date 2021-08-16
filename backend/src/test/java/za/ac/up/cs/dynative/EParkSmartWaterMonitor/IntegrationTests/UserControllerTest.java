470611a209695c686c87aa296adebd325567e090
JoanitaNell <u19006812@tuks.co.za>
2021-08-16 12:03:14 +0200

    User Integration tests:   --> creating users with incomplete requests

--- backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/IntegrationTests/UserControllerTest.java
+++ backend/src/test/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/IntegrationTests/UserControllerTest.java
@@ -8,6 +8,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.user.requests.CreateUserRequest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.user.responses.CreateUserResponse;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.GetSiteByIdRequest;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.GetSiteByIdResponse;
+
 import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +25,82 @@ public class UserControllerTest {
     private TestRestTemplate restTemplate;
 
     //post: /api/user/createUser
+    @Test
+    public void createUserIncompleteDetails(){
+        UUID parkId=UUID.randomUUID();
+        String idNumber = "2356897410000";
+        String name = "Nita";
+        String surname = "Nell";
+        String email = "utests@dynative.com";
+        String password = "request.getPassword()";
+        String username = "chichi";
+        String role = "ADMIN";
+        String cellNumber = "1234567890";
+
+        //test 1:
+        CreateUserRequest request= new CreateUserRequest(parkId,idNumber,email,password,name,surname,"",role,cellNumber);
+        ResponseEntity<CreateUserResponse> response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+
+        //test 2:
+        request= new CreateUserRequest(parkId,"",email,password,name,surname,"",role,cellNumber);
+        response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+
+        //test 3:
+        request= new CreateUserRequest(null,idNumber,email,password,name,surname,username,role,cellNumber);
+        response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+
+        //test 4:
+        request= new CreateUserRequest(parkId,idNumber,email,password,name,surname,username,role,"");
+        response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+
+        //test 5:
+        request= new CreateUserRequest(parkId,idNumber,email,password,name,surname,username,"",cellNumber);
+        response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+
+        //test 6:
+        request= new CreateUserRequest(parkId,idNumber,email,password,name,"",username,role,cellNumber);
+        response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+
+        //test 7:
+        request= new CreateUserRequest(parkId,idNumber,"",password,name,surname,username,role,cellNumber);
+        response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+
+        //test 8:
+        request= new CreateUserRequest(parkId,idNumber,email,password,"",surname,username,role,cellNumber);
+        response = restTemplate.withBasicAuth("ChiChiTestingADMIN","dynativeNext")
+                .postForEntity("/api/user/createUser", request,CreateUserResponse.class);
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals("User's details are incomplete",response.getBody().getStatus());
+        assertEquals(false,response.getBody().getSuccess());
+    }
 
     //post: /api/user/deleteUser
 
