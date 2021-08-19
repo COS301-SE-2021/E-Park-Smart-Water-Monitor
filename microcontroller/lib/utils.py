7a8e68e9c2184a0fe8b69b4193c631d9fb410496
Abel Jansen <abeljansenbf@gmail.com>
2021-08-19 14:16:33 +0200

    Added exceptions class and settime method to utils

--- microcontroller/lib/utils.py
+++ microcontroller/lib/utils.py
@@ -1,4 +1,11 @@
-import network
+import time
+import ntptime
+import utime
+import machine
+import logger
+
+class CustomException(Exception):
+    pass
 
 def connect_wifi(ssid, pw):
     try:
@@ -20,3 +27,15 @@ def connect_wifi(ssid, pw):
         
     except Exception as e:
         raise CustomException("ERROR in connect_wifi(): " + str(e))
+
+def set_time():
+    try:
+        logger.log('Setting time...')
+        tm = utime.gmtime(ntptime.time())
+        machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3] + 2, tm[4], tm[5], 0))
+        logger.log('Time is set!')
+        
+    except Exception as e:
+        print(e.message)
+        raise CustomException("ERROR in set_time(): " + str(e))
+
