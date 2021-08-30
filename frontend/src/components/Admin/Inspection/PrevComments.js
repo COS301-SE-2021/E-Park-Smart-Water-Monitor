1dffbc8e087227c9a71edc7827635d67b1d529d2
JoanitaNell <u19006812@tuks.co.za>
2021-08-30 19:10:54 +0200

    comments working 100% now, thank goooodness

--- frontend/src/components/Admin/Inspection/PrevComments.js
+++ frontend/src/components/Admin/Inspection/PrevComments.js
@@ -1,126 +1,45 @@
 import React, {useContext, useEffect, useState} from "react";
-import {Form} from 'react-bootstrap';
-
 import { makeStyles } from "@material-ui/core/styles";
 import componentStyles from "assets/theme/views/admin/admin";
 import "../../../assets/css/addDevice.css";
-import {UserContext} from "../../../Context/UserContext";
-import {TextareaAutosize} from "@material-ui/core";
-import { View, Text } from "react";
-
 
 const useStyles = makeStyles(componentStyles);
 
-
 const PrevComments = (props) => {
-    const user = useContext(UserContext)
     const [response, setResponse] = useState([])
 
-
-    useEffect(() => {
-        let com
-        let m = props.comments.map((comment) => { com= comment.split("\n")
-        return(
-            <>
-                <label>{com[0]}</label>
-                <br/>
-                <TextareaAutosize value={com[1]}/>
-                <br/><br/>
-
-
-
-
-            </>
-        )})
-       setResponse(m)
-    },[])
-
-    /*useEffect(() => {
+    useEffect(()=>{
         let com
         let m = props.comments.map((comment) => {
             com = comment.split("\n")
-            if (com[0] == props.user) { //sent a message
-                return (
-                    <View style={{
-                        backgroundColor: "#0078fe",
-                        padding: 10,
-                        marginLeft: '45%',
-                       // borderRadius: 5,
-
-                        marginTop: 5,
-                        marginRight: "5%",
-                        maxWidth: '50%',
-                        alignSelf: 'flex-end',
-                        borderRadius: 20,
-                    }}>
-
-                        <Text style={{fontSize: 16, color: "#fff",}}> {com[1]}</Text>
-                        <View style={{
-                            position: "absolute",
-                            backgroundColor: "#0078fe",
-                            //backgroundColor:"red",
-                            width: 20,
-                            height: 25,
-                            bottom: 0,
-                            borderBottomLeftRadius: 25,
-                            right: -10
-                        }}>
-                        </View>
-                        <View style={{
-                            position: "absolute",
-                            backgroundColor: "#eeeeee",
-                            //backgroundColor:"green",
-                            width: 20,
-                            height: 35,
-                            bottom: -6,
-                            borderBottomLeftRadius: 18,
-                            right: -20
-                        }}></View>
-                    </View>
-                )
-            } else {
-                return ( //not sent by user currently logged in
-                    <View style={{
-                        backgroundColor: "#dedede",
-                        padding: 10,
-                        //borderRadius: 5,
-                        marginTop: 5,
-                        marginLeft: "5%",
-                        maxWidth: '50%',
-                        alignSelf: 'flex-start',
-                        //maxWidth: 500,
-                        //padding: 14,
-
-                        //alignItems:"center",
-                        borderRadius: 20,
-                    }}>
-                        <Text style={{fontSize: 16, color: "#000", justifyContent: "center"}}> {com[1]}</Text>
-                        <View style={{
-                            position: "absolute",
-                            backgroundColor: "#dedede",
-                            width: 20,
-                            height: 25,
-                            bottom: 0,
-                            borderBottomRightRadius: 25,
-                            left: -10
-                        }}>
-                        </View>
-                        <View style={{
-                            position: "absolute",
-                            backgroundColor: "#eeeeee",
-                            width: 20,
-                            height: 35,
-                            bottom: -6,
-                            borderBottomRightRadius: 18,
-                            left: -20
-                        }}></View>
-                    </View>
-                )
+            if (com[0] == props.user+":"){
+                return(
+                    <div style={{
+                    backgroundColor: "#4c7870",
+                    padding: 10,
+                    marginLeft: '50%',
+                    marginTop: 5,
+                    maxWidth: '50%',
+                    alignSelf: 'flex-end',
+                    borderRadius: 15,
+                }}> <b><h9 style={{color: "#fff"}}> {com[0]}</h9></b>
+                    <p style={{fontSize: 16, color: "#fff",overflowWrap: "break-word"}}> {com[1]}</p>
+                </div>)
+            }else{
+                return(<div style={{
+                    backgroundColor: "#dedede",
+                    padding: 10,
+                    marginTop: 5,
+                    maxWidth: '50%',
+                    alignSelf: 'flex-start',
+                    borderRadius: 15,
+                }}><b><h9 style={{color: "#000000"}}> {com[0]}</h9></b>
+                    <p style={{fontSize: 16, color: "#000000", justifyContent: "center", overflowWrap: "break-word"}}> {com[1]}</p>
+                </div>)
             }
         })
-
         setResponse(m)
-    },[])*/
+    },[])
 
     return (
         <>
