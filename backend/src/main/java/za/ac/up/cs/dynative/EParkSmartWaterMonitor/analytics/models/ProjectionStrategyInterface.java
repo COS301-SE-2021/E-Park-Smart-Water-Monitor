dde06807ce869fc2e32486fe4546f8a1a652dcc1
Janlu <u16014520@tuks.co.za>
2021-09-03 17:29:29 +0200

    Addition of the some code documentation ProjectionStrategyInterface

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/analytics/models/ProjectionStrategyInterface.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/analytics/models/ProjectionStrategyInterface.java
@@ -3,7 +3,7 @@ package za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics.models;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics.responses.DeviceProjectionResponse;
 
 public interface ProjectionStrategyInterface {
-    /*
+    /**
         ProjectionStrategyInterface serves as the interface class for the predictive aspect of the project.
         It serves as the main "blueprint" of what functions will be needed to facilitate the prediction of
         - water level
