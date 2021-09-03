c0393e7de33537088bc78bfdd5e7ed96eb0cbc31
Janlu <u16014520@tuks.co.za>
2021-09-03 17:27:21 +0200

    Changing of the service implementation for projections

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/analytics/AnalyticsServiceImpl.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/analytics/AnalyticsServiceImpl.java
@@ -1,6 +1,8 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics.models.*;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.analytics.models.WaterLevelProjectionStrategy;
@@ -16,102 +18,121 @@ import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.FindWater
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.devices.responses.GetDeviceDataResponse;
 
 @Service("AnalyticsServiceImpl")
-public class AnalyticsServiceImpl implements AnalyticsService {
+public class AnalyticsServiceImpl implements AnalyticsService
+{
 
+    @Value("${weather.apikey}")
+    private String apikey;
+    @Value("${weather.url}")
+    private String weatherUrl;
     private final DevicesService devicesService;
     private final WaterSiteService waterSiteService;
     private ProjectionStrategyInterface projectionStrategy;
 
+    @Autowired
     public AnalyticsServiceImpl(@Qualifier("DeviceServiceImpl") DevicesService devicesService,
-                                @Qualifier("WaterSiteServiceImpl") WaterSiteService waterSiteService) {
+                                @Qualifier("WaterSiteServiceImpl") WaterSiteService waterSiteService)
+
+    {
         this.devicesService = devicesService;
         this.waterSiteService = waterSiteService;
     }
 
+    /**
+     * AnalyticsServiceImpl/deviceProjection acts as the entry point for the Prediction Strategy.
+     * Provides initial error checking
+     * Act as the entry point for the various prediction strategies.
+     * Temperature Prediction Strategy
+     * Water Quality Prediction Strategy
+     * Water Level Prediction Strategy
+     *
+     * @param request DeviceProjectionRequest object. Provides the length of the prediction i.e. days into the
+     *                future user wants a prediction for.
+     *                String type of projection they want. "waterlevel", "ph", "temperature"
+     *                UUID id of the specific device(which monitors a water site) they want a prediction for
+     *
+     * @return returns a DeviceProjectionResponse which contains
+     * 3 arrays of length n where n is max 30 or n is equal to the number of days a device has measurements for
+     * plus the length of the prediction
+     * optimistic = []
+     * realistic = []
+     * conservative = []
+     * labelDates = []
+     * and some other peripheral variables
+     */
     @Override
-    public DeviceProjectionResponse deviceProjection(DeviceProjectionRequest request) {
-        if (request.getId() != null) {
-
+    public DeviceProjectionResponse deviceProjection(DeviceProjectionRequest request)
+    {
+        if (request.getId() != null)
+        {
             FindDeviceResponse findDeviceResponse = devicesService.findDevice(new FindDeviceRequest(request.getId()));
-            FindWaterSiteByDeviceResponse waterSiteByDeviceResponse = waterSiteService.findWaterSiteByDeviceId(
-                    new FindWaterSiteByDeviceRequest(request.getId()));
+            FindWaterSiteByDeviceResponse waterSiteByDeviceResponse = waterSiteService.findWaterSiteByDeviceId(new FindWaterSiteByDeviceRequest(request.getId()));
 
             if ((findDeviceResponse.getSuccess() && findDeviceResponse.getDevice() != null)
-                    && (waterSiteByDeviceResponse.getSuccess() && waterSiteByDeviceResponse.getWaterSite() != null)) {
+                    && (waterSiteByDeviceResponse.getSuccess() && waterSiteByDeviceResponse.getWaterSite() != null))
+            {
+                /*
+                Take note:
+                    The sorted boolean should be set to true once in production.
+                    It is set to false, because we have to simulate timeseries data (data over a period of time).
+                    This is done by changing the dates on the measurements a device sends in.
 
+                    0 returns all data of a device.
+                    true returns the latest data first.
+                */
                 GetDeviceDataResponse deviceDataResponse = devicesService.getDeviceData(
                         new GetDeviceDataRequest(findDeviceResponse.getDevice().getDeviceName(),0, false));
 
-                if (deviceDataResponse.getSuccess()) {
-                    if (request.getLength() >= 0 && request.getLength() <= 10) {
-                        switch (request.getType()) {
+                if (deviceDataResponse.getSuccess())
+                {
+                    if (request.getLength() >= 0 && request.getLength() <= 20)
+                    {
+                        switch (request.getType().toLowerCase())
+                        {
                             case "ph":
                                 projectionStrategy = new PhProjectionStrategy(request, deviceDataResponse);
                                 return projectionStrategy.predict();
                             case "waterlevel":
                                 projectionStrategy = new WaterLevelProjectionStrategy(request,
                                         deviceDataResponse,
-                                        waterSiteByDeviceResponse);
+                                        waterSiteByDeviceResponse,
+                                        apikey,
+                                        weatherUrl);
                                 return projectionStrategy.predict();
                             case "temperature":
                                 projectionStrategy = new TemperatureProjectionStrategy(request, deviceDataResponse);
                                 return projectionStrategy.predict();
                             default:
-                                return new DeviceProjectionResponse(
-                                    "Invalid prediction strategy specified, must be of type temperature, ph or waterlevel",
-                                    false,
-                                    request.getType(),
-                                    request.getLength(),
-                                    null,
-                                    null,
-                                    null,
-                                    null);
+                                return failedProjection("Invalid prediction strategy specified, must be of type temperature, ph or waterlevel", request.getType(), request.getLength());
                         }
                     }
-                    else {
-                        return new DeviceProjectionResponse(
-                                "Invalid prediction length specified, must be within the range 0-10.",
-                                false,
-                                request.getType(),
-                                request.getLength(),
-                                null,
-                                null,
-                                null,
-                                null);
+                    else
+                    {
+                        return failedProjection("Invalid prediction length specified, must be within the range 0-10.", request.getType(), request.getLength());
                     }
                 }
-                else {
-                    return new DeviceProjectionResponse(
-                            deviceDataResponse.getStatus(),
-                            false,
-                            request.getType(),
-                            request.getLength(),
-                            null,
-                            null,
-                            null,
-                            null);
+                else
+                {
+                    return failedProjection(deviceDataResponse.getStatus(), request.getType(), request.getLength());
                 }
             }
-            else {
-                return new DeviceProjectionResponse(
-                        findDeviceResponse.getStatus(),
-                        false,
-                        request.getType(),
-                        request.getLength(),
-                        null,
-                        null,
-                        null,
-                        null);
+            else
+            {
+                return failedProjection("Could not get device data or the water site it monitors.", request.getType(), request.getLength());
             }
         }
-        return new DeviceProjectionResponse(
-                "No device id specified",
-                false,
-                request.getType(),
-                request.getLength(),
-                null,
-                null,
-                null,
-                null);
+        return failedProjection("No device id specified", request.getType(), request.getLength());
+    }
+
+    /**
+     * Utility method
+     * @param status string of the failed projection
+     * @param type string of what type of projection they attempted
+     * @param length integer of what period a user wants to predict for
+     * @return DeviceProjectionResponse object
+     */
+    public DeviceProjectionResponse failedProjection(String status, String type, int length)
+    {
+        return new DeviceProjectionResponse(status, false, type, length, null, null, null, null);
     }
 }
