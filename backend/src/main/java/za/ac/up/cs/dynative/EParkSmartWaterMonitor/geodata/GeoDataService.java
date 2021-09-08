dd2e1273c6485aff28fa146bc2c72010604c5daf
TheySeeMeRolan <u16002513@tuks.co.za>
2021-09-08 23:43:04 +0200

    GeoData interface updated

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/geodata/GeoDataService.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/geodata/GeoDataService.java
@@ -1,13 +1,19 @@
 package za.ac.up.cs.dynative.EParkSmartWaterMonitor.geodata;
 
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.geodata.models.Coordinate;
+import za.ac.up.cs.dynative.EParkSmartWaterMonitor.geodata.responses.GetElevationDataResponse;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public interface GeoDataService
 {
     public void loadElevation(Coordinate startingLocation, double SquaredKm);
     public void lineApproximation(Point from, Point to);
     public Point convertCoordToGridBlock(Coordinate inCoordinate);
+    public Coordinate convertGridBlockToCoord(int x,int y);
+    public ArrayList<ArrayList<Double>> geoSquareBuilder(Coordinate coordinate);
+    public GetElevationDataResponse getElevationData();
+
 
 }
