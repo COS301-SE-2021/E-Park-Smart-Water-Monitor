f26911c4e0622bcafdd8da94bf2d541f6cd179f6
TheySeeMeRolan <u16002513@tuks.co.za>
2021-09-08 22:34:05 +0200

    geoFeatures implemented

---
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/geodata/models/geoFeatures.java
@@ -0,0 +1,40 @@
+package za.ac.up.cs.dynative.EParkSmartWaterMonitor.geodata.models;
+
+import java.util.ArrayList;
+
+public class geoFeatures {
+    private String type;
+    private FeatureProperties properties;
+    private ArrayList<ArrayList<ArrayList<Double>>> coordinates;
+
+    public geoFeatures(FeatureProperties properties, ArrayList<ArrayList<ArrayList<Double>>> coordinates)
+    {
+        this.type = "geometry";
+        this.properties = properties;
+        this.coordinates = coordinates;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public FeatureProperties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(FeatureProperties properties) {
+        this.properties = properties;
+    }
+
+    public ArrayList<ArrayList<ArrayList<Double>>> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(ArrayList<ArrayList<ArrayList<Double>>> coordinates) {
+        this.coordinates = coordinates;
+    }
+}
