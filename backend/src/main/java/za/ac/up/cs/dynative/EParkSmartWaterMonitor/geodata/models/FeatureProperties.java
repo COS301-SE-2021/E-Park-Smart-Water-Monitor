eab25836681e5ee9f18ea2e4dcb7e9e3d6bbf49c
TheySeeMeRolan <u16002513@tuks.co.za>
2021-09-12 13:30:26 +0200

    feature properties adjusted

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/geodata/models/FeatureProperties.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/geodata/models/FeatureProperties.java
@@ -2,9 +2,14 @@ package za.ac.up.cs.dynative.EParkSmartWaterMonitor.geodata.models;
 
 public class FeatureProperties {
     private double dataValue;
+    private String fill;
+    private String stroke;
 
-    public FeatureProperties(double dataValue) {
+    public FeatureProperties(double dataValue, String colour)
+    {
         this.dataValue = dataValue;
+        this.fill = colour;
+        this.stroke = colour;
     }
 
     public double getDataValue() {
@@ -13,5 +18,21 @@ public class FeatureProperties {
 
     public void setDataValue(double dataValue) {
         this.dataValue = dataValue;
+    }
+
+    public String getFill() {
+        return fill;
+    }
+
+    public void setFill(String fill) {
+        this.fill = fill;
+    }
+
+    public String getStroke() {
+        return stroke;
+    }
+
+    public void setStroke(String stroke) {
+        this.stroke = stroke;
     }
 }
