bbc427cd5c11933b2e4bbfcab44995a9493ca702
Janlu <u16014520@tuks.co.za>
2021-09-12 20:53:51 +0200

    Flask server for arima model

--- lambdaFunctions/arima.py
+++ lambdaFunctions/arima.py
@@ -1,31 +1,39 @@
+  GNU nano 4.8                                              arima_server.py
+from flask import Flask, request, jsonify, redirect, url_for, render_template
 from statsmodels.tsa.ar_model import AR
 from statsmodels.tsa.arima_model import ARIMA
 import warnings
 warnings.filterwarnings('ignore')
-import sys
 
-phReadings = list()
-for i in range(1,len(sys.argv)):
-    phReadings.append(float(sys.argv[i].replace(",","")))
+app = Flask(__name__)
 
-training = [val for val in phReadings]
-predictions = list()
-numPredictions = 5
-for num in range(numPredictions):
-    try:
-        model = ARIMA(training, order=(1,0,1))
-        mode_fit = model.fit(disp=-1)
-        forecast = mode_fit.forecast()[0]
-        phReadings.append(forecast[0])
+@app.route("/")
+def home():
+        return "Home."
 
-        if num % 5 == 0 and num > 0:
-            training.append(training[num])
-        else:
-            training.append(forecast)
-    except:
-        continue
+@app.route("/arimaPrediction",methods=["POST"])
+def predict():
+        phReadings = request.json["phLevels"].split(",")
+        training = [val for val in phReadings]
+        predictions = list()
+        numPredictions = 5
+        for num in range(numPredictions):
+                try:
+                        model = ARIMA(training, order=(1,0,1))
+                        model_fit = model.fit(disp=-1)
+                        forecast = model_fit.forecast()[0]
+                        phReadings.append(forecast[0])
 
-print(phReadings)
+                        if num % 5 == 0 and num > 0:
+                                training.append(training[num])
+                        else:
+                                training.append(forecast[0])
+                except:
+                        continue
+        phReadings = str(phReadings)
+        return phReadings
 
-# install statsmodels version 0.10.0
-# install numpy version 1.16.5
+if __name__ == "__main__":
+        app.run(host = '0.0.0.0',port=5000,debug=True)
+
+
