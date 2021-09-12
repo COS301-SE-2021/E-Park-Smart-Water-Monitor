12f7b245fa2e21c2e42fcb272d01e5692073ab06
Michaela Schormann <u19014938@tuks.co.za>
2021-09-12 22:47:19 +0200

    Revert "test cors issue"
    This reverts commit ea1c31ecb5551f7536d88e957af6dabdc1a2184b.

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/controller/WaterSiteController.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/controller/WaterSiteController.java
@@ -15,7 +15,7 @@ import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.EditWaterS
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.GetSiteByIdRequest;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.GetSiteByIdResponse;
 
-@CrossOrigin(origins = "http://localhost:300")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/sites")
 public class WaterSiteController {
