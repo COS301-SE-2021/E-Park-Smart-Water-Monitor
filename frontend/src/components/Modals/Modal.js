a94d8a3f248ee0169a7d0cd95c6936b02f29439c
Michaela Schormann <u19014938@tuks.co.za>
2021-09-18 17:43:39 +0200

    Trying to get the modals to behave differently

--- frontend/src/components/Modals/Modal.js
+++ frontend/src/components/Modals/Modal.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import "../../assets/css/modal.css";
 
 const Modal = props => {
-
+    const [result, setResult] = useState("")
 
     const closeOnEscapeKeyDown = e => {
         if ((e.charCode || e.keyCode) === 27) {
@@ -13,24 +13,10 @@ const Modal = props => {
     };
 
     useEffect(() => {
+
         if(props.title)
         {
-            document.body.addEventListener("keydown", closeOnEscapeKeyDown);
-            return function cleanup() {
-                document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
-            };
-        }
-
-    }, []);
-
-
-    return ReactDOM.createPortal(
-        <CSSTransition
-            in={props.show}
-            unmountOnExit
-            timeout={{ enter: 0, exit: 300 }}
-        >
-             <div className="modal" >
+            let m =  <div className="modal" onClick={ props.onClose() }>
                 <div className="modal-content" onClick={e => e.stopPropagation()}>
                     { props.title && <div className="modal-header">
 
@@ -42,6 +28,34 @@ const Modal = props => {
                     <div className="modal-body">{props.children}</div>
                 </div>
             </div>
+
+            setResult(m)
+        }else{
+            alert("loader")
+            let m =  <div className="modal" >
+                <div className="modal-content" onClick={e => e.stopPropagation()}>
+                    <div className="modal-body">{props.children}</div>
+                </div>
+            </div>
+
+            setResult(m)
+        }
+
+        document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+        return function cleanup() {
+            document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+        };
+
+    }, []);
+
+
+    return ReactDOM.createPortal(
+        <CSSTransition
+            in={props.show}
+            unmountOnExit
+            timeout={{ enter: 0, exit: 300 }}
+        >
+            { result }
         </CSSTransition>,
         document.getElementById("root")
     );
