0daa62cd79ab2c1540a969c254db2113c60aa7d4
JoanitaNell <u19006812@tuks.co.za>
2021-10-04 20:04:46 +0200

    Documentation of the water site service ✈️

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/watersite/Site.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/watersite/Site.java
@@ -11,34 +11,59 @@ import java.util.Set;
 
 import static org.springframework.data.neo4j.core.schema.Relationship.Direction.OUTGOING;
 
+/**
+ * The class represents a site within the database, hence the @Node annotation.
+ */
 @Node
-public class Site
-{
+public class Site {
+
+    /**
+     * Attributes:
+     */
     @Id
     @GeneratedValue
     private  Long siteId;
     private  String siteName;
 
-
+    /**
+     * The relationship that needs to be maintained between nodes.
+     */
     @Relationship(type = "WATER_MONITORED_BY", direction = OUTGOING)
     private Set<Device> devices = new HashSet<>();
 
-
-    public Site(String sName)
-    {
+    /**
+     * Custom constructor of the Site.
+     * @param sName The name of the site.
+     */
+    public Site(String sName) {
         this.siteName=sName;
     }
 
+    /**
+     * Default constructor.
+     */
     public Site() {}
 
+    /**
+     * Get the id of a water site.
+     * @return A long (integer) value will be returned with the id, that in turn will be converted into the UUID.
+     */
     public Long getSiteId() {
         return siteId;
     }
 
+    /**
+     * Retrieve the name of the Site by giving the id.
+     * @return Return the name of the specified water site.
+     */
     public String getSiteName() {
         return siteName;
     }
 
+    /**
+     * Method to add a new water source device to the database.
+     * @param newDevice The Device object and all its details.
+     */
     public void addWaterSourceDevice(Device newDevice) {
         if (devices == null) {
             devices = new HashSet<>();
--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/watersite/WaterSiteService.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/watersite/WaterSiteService.java
@@ -27,13 +27,59 @@ public interface WaterSiteService {
      * @return the request completion status and success value is returned in the object.
      */
     AttachWaterSourceDeviceResponse attachWaterSourceDevice(AttachWaterSourceDeviceRequest request) throws InvalidRequestException;
+
+    /**
+     * Test to determine if it is vallid to attach a water source device.
+     * @param request The CanAttachWaterSourceDeviceRequest created in the attachWaterSourceDevice method.
+     * @return return a response of the request.
+     */
     CanAttachWaterSourceDeviceResponse canAttachWaterSourceDevice(CanAttachWaterSourceDeviceRequest request) throws InvalidRequestException;
+
+    /**
+     * This method will get the water site of the id specified or null if it does not exist.
+     * @param request The request containing the site id.
+     * @return A response containing the site.
+     */
     GetSiteByIdResponse getSiteById(GetSiteByIdRequest request) throws InvalidRequestException;
+
+    /**
+     * Saving a node in the database.
+     * @param request The object containing the details of the water site that is being saved within the database.
+     * @return The response of the request indicating the success of the request.
+     */
     SaveSiteResponse saveSite(SaveSiteRequest request);
+
+    /**
+     * Deleting a water site from the system.
+     * @param deleteWaterSiteRequest The request contains the id of the water site to be deleted.
+     * @return A response will be returned indicting the success of the request.
+     */
     DeleteWaterSiteResponse deleteWaterSite(DeleteWaterSiteRequest deleteWaterSiteRequest);
+
+    /**
+     * Edit an existing water site in the system.
+     * @param editWaterSiteRequest The water site details to be updated.
+     * @return A response containing the success value and status will be returned.
+     */
     EditWaterSiteResponse editWaterSite(EditWaterSiteRequest editWaterSiteRequest);
+
+    /**
+     * Find a water site by its id.
+     * @param findWaterSiteByDeviceRequest the object contains the ID of the water site.
+     * @return A response object containing the water site will be returned.
+     */
     FindWaterSiteByDeviceResponse findWaterSiteByDeviceId(FindWaterSiteByDeviceRequest findWaterSiteByDeviceRequest);
+
+    /**
+     * Retrieve the water site via the devices connected to it.
+     * @param id The id of a device connected to the water site.
+     * @return A watersite will be returned.
+     */
     WaterSite getWaterSiteByRelatedDevice( UUID id);
+
+    /**
+     * This method is only used for internal testing purposes.
+     */
     DeleteWaterSiteInternalResponse deleteInternal(DeleteWaterSiteInternalRequest request);
 }
 
