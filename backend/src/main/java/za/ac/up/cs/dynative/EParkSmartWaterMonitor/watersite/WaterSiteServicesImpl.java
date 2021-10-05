cde2d658f85367470d23b57bb2c8e76ac3386dc4
JoanitaNell <u19006812@tuks.co.za>
2021-10-05 10:18:30 +0200

    Documentation of the water site service implementation ✈️

--- backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/watersite/WaterSiteServicesImpl.java
+++ backend/src/main/java/za/ac/up/cs/dynative/EParkSmartWaterMonitor/watersite/WaterSiteServicesImpl.java
@@ -7,7 +7,6 @@ import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.requests.FindWaterS
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.FindWaterSiteByDeviceResponse;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.ParkServiceImpl;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.requests.FindByParkIdRequest;
-import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.requests.SaveParkRequest;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.park.responses.FindByParkIdResponse;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.models.WaterSite;
 import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.repositories.WaterSiteRepo;
@@ -16,18 +15,37 @@ import za.ac.up.cs.dynative.EParkSmartWaterMonitor.watersite.responses.*;
 
 import java.util.*;
 
+/**
+ * This class is the implementation of the Water Site Service Interface.
+ */
 @Service("WaterSiteServiceImpl")
-public class WaterSiteServicesImpl implements WaterSiteService
-{
+public class WaterSiteServicesImpl implements WaterSiteService {
+    /**
+     * Attributes initialized with the @Autowired annotation constructor.
+     */
     ParkServiceImpl parkService;
     WaterSiteRepo waterSiteRepo;
 
+    /**
+     * Constructor to initialize the attributes
+     * @param parkService
+     * @param sRepo
+     */
     @Autowired
     public WaterSiteServicesImpl(@Qualifier("ParkService") ParkServiceImpl parkService, @Qualifier("WaterSiteRepo") WaterSiteRepo sRepo) {
         this.waterSiteRepo = sRepo;
         this.parkService = parkService;
     }
 
+    /**
+     * This method is used to add a water site to park in the system.
+     * <p>
+     *     A site can only be added if the park id is specified and is valid, and the shape is correct.
+     * </p>
+     * @param request of type AddSiteRequest will contain the park id and site information.
+     * @return An AddSiteResponse is returned, indicating the success status and value of the request,
+     * an id of the new site is also in the returned object.
+     */
     @Override
     public AddSiteResponse addSite(AddSiteRequest request) {
         AddSiteResponse response = new AddSiteResponse();
@@ -39,15 +57,6 @@ public class WaterSiteServicesImpl implements WaterSiteService
         if (request.getParkId() != null) {
             if (Objects.equals(request.getShape(), "circle") || Objects.equals(request.getShape(), "rectangle")
                     && (request.getLength() >= 0 && request.getWidth() >= 0 && request.getRadius() >= 0)) {
-//                WaterSite waterSite = new WaterSite(UUID.randomUUID(),
-//                        request.getSiteName(),
-//                        request.getLatitude(),
-//                        request.getLongitude(),
-//                        request.getShape(),
-//                        request.getLength(),
-//                        request.getWidth(),
-//                        request.getRadius());
-
                 FindByParkIdResponse findByParkIdResponse = parkService.findByParkId(new FindByParkIdRequest(request.getParkId()));
 
                 if (findByParkIdResponse != null) {
@@ -79,6 +88,11 @@ public class WaterSiteServicesImpl implements WaterSiteService
         return response;
     }
 
+    /**
+     * This method tests if it is possible to add a water site to a park.
+     * @param request The CanAttachWaterSourceDeviceRequest created in the attachWaterSourceDevice method.
+     * @return An CanAttachWaterSourceDeviceResponse is returned indicating the success status and value of the request.
+     */
     public CanAttachWaterSourceDeviceResponse canAttachWaterSourceDevice(CanAttachWaterSourceDeviceRequest request)  {
         CanAttachWaterSourceDeviceResponse response;
         if (request==null){
@@ -98,6 +112,11 @@ public class WaterSiteServicesImpl implements WaterSiteService
         return response;
     }
 
+    /**
+     * Method to attach a new device to a site.
+     * @param request will contain the device to be attached along with the site id.
+     * @return An AttachWaterSourceDeviceResponse will be returned indicating the success of the request.
+     */
     public AttachWaterSourceDeviceResponse attachWaterSourceDevice(AttachWaterSourceDeviceRequest request)  {
         AttachWaterSourceDeviceResponse response;
         if (request.getSiteId()==null){
@@ -127,6 +146,11 @@ public class WaterSiteServicesImpl implements WaterSiteService
         return null;
     }
 
+    /**
+     * Retrieve a water site based on a specified id.
+     * @param request The request containing the site id.
+     * @return GetSiteByIdResponse object is returned containing the water site.
+     */
     @Override
     public GetSiteByIdResponse getSiteById(GetSiteByIdRequest request)  {
         GetSiteByIdResponse response;
@@ -143,6 +167,11 @@ public class WaterSiteServicesImpl implements WaterSiteService
         return response;
     }
 
+    /**
+     * Saving the water site (new or existing into the database)
+     * @param request The object containing the details of the water site that is being saved within the database.
+     * @return A SaveWaterSiteResponse is returned indicating the success of the reqyest.
+     */
     @Override
     public SaveSiteResponse saveSite(SaveSiteRequest request) {
         if (request.getSite() != null) {
@@ -154,6 +183,11 @@ public class WaterSiteServicesImpl implements WaterSiteService
         }
     }
 
+    /**
+     * Used to delete a water site and its related devices from the system.
+     * @param request indicating which water site is to be deleted.
+     * @return A DeleteWaterSiteResponse indicating the success of the request is returned.
+     */
     @Override
     public DeleteWaterSiteResponse deleteWaterSite(DeleteWaterSiteRequest request) {
         System.out.println(request.getWaterSiteId());
@@ -168,6 +202,11 @@ public class WaterSiteServicesImpl implements WaterSiteService
         return new DeleteWaterSiteResponse("No watersite with this id exists.", false);
     }
 
+    /**
+     * Update an existing water site. The name and coordinates are updatable.
+     * @param request a request containing the water site details along with the new details to be updated.
+     * @return An EditWaterSiteResponse is returned containing the success value and status of the request.
+     */
     @Override
     public EditWaterSiteResponse editWaterSite(EditWaterSiteRequest request) {
         EditWaterSiteResponse response = new EditWaterSiteResponse();
@@ -199,6 +238,11 @@ public class WaterSiteServicesImpl implements WaterSiteService
         return response;
     }
 
+    /**
+     * Retrieve a watersite based on a device ID.
+     * @param request contains the ID to base the search on
+     * @return A watersite along with the success status and value is encapsulated and returned as a FindWaterSiteByDeviceResponse object.
+     */
     @Override
     public FindWaterSiteByDeviceResponse findWaterSiteByDeviceId(FindWaterSiteByDeviceRequest request) {
         if (request == null) {
@@ -215,14 +259,20 @@ public class WaterSiteServicesImpl implements WaterSiteService
             return new FindWaterSiteByDeviceResponse("Watersite not found",false,null);
     }
 
-    public WaterSite getWaterSiteByRelatedDevice(UUID id)
-    {
+    /**
+     * Retrieve a water site based on a device connected to it.
+     * @param id The id of a device connected to the water site.
+     * @return The retrieved Watersite will be returned.
+     */
+    public WaterSite getWaterSiteByRelatedDevice(UUID id) {
         return  waterSiteRepo.getWaterSiteByRelatedDevice(id);
     }
 
+    /**
+     * This method is only used for internal purposes.
+     */
     public DeleteWaterSiteInternalResponse deleteInternal(DeleteWaterSiteInternalRequest request){
         waterSiteRepo.deleteById(request.getId());
-        //System.out.println(request.getId());
         return new DeleteWaterSiteInternalResponse(true);
     }
 
