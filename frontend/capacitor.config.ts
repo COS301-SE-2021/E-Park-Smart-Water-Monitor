6ae5013e56bb58b90ccedb3cbac8d1ce1a6f2ea5
Michaela Schormann <u19014938@tuks.co.za>
2021-10-13 18:54:12 +0200

    Added Android files
    Readded capacitor initialisation

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.epark.www',
   appName: 'epark-water-monitoring',
   webDir: 'build',
   bundledWebRuntime: false
